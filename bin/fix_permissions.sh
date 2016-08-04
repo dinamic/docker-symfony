960b8dc86816decb153622e40807d3539e23b5b0
Nikola Petkanski <nikola@petkanski.com>
2016-08-04 23:23:38 +0300

    ~ updated bin/fix_permissions.sh to fix a few more scenarios where things can go wrong

--- bin/fix_permissions.sh
+++ bin/fix_permissions.sh
@@ -2,5 +2,5 @@
 
 DOCKER_PHP_ID=$(docker ps | grep docker_php | awk '{print $1}')
 
-docker exec -it $DOCKER_PHP_ID sh -c "chown -R www-data:www-data /var/www/symfony/app/logs"
-docker exec -it $DOCKER_PHP_ID sh -c "chmod -R 755 /var/www/symfony/app/logs"
+docker exec -it $DOCKER_PHP_ID sh -c "chown -R www-data:www-data ./app ./src ./vendor"
+docker exec -it $DOCKER_PHP_ID sh -c "chmod -R 755 ./app/logs"
